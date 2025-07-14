7de0df61c5742d4e82a02245b6f52e01a972a6b4
Volodya Shulga <volodyavshulga@gmail.com>
2025-07-14 23:24:12 +0000

    Fixed base.pp spacing

--- modules/profile/manifests/base.pp
+++ modules/profile/manifests/base.pp
@@ -1,7 +1,23 @@
 class profile::base {
-  notify {'Applying profile::base to this node!': }                                                                                                                                                                                                # Make sure vim is installed                                                                                            package { 'vim':
-    ensure => installed,                                                                                                  }                                                                                                                     
+  notify {'Applying profile::base to this node!': }
 
-  # Create user volodya                                                                                                   user { 'volodya':
-    ensure => present,                                                                                                      shell => /bin/bash,                                                                                                     managehome => true,                                                                                                   }                                                                                                                                                                                                                                               # Set up motd                                                                                                           file { '/etc/motd':                                                                                                       content: "Welcome to my puppet managed project",
-    ensure => file,                                                                                                         owner => 'root',                                                                                                        group => 'root',                                                                                                        mode => '0644',                                                                                                       }                                                                                                                     }
+  # Make sure vim is installed
+  package { 'vim':
+    ensure => present,
+  }
+
+  #Set up motd
+  file { '/etc/motd':
+    ensure => file,
+    content => "Welcome to my puppet managed project",
+    owner => 'root',
+    group => 'root',
+    mode => '0644',
+
+  # Create user volodya
+  user { 'volodya':
+    ensure => present,
+    shell => '/bin/bash',
+    managehome => true,
+  }
+}
